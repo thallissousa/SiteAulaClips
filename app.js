f202ab85303f3d0e30340d69ab1e3d3d862037f3
thallissousa <thallissousa@outlook.com>
2021-11-23 16:10:43 -0300

    corrigindo acesso

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use('/', express.static(__dirname + '/public'));
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
